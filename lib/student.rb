8e79d475816614503b86eac794b3f18d8bb1826b
DispicableLee <lee.robert053@gmail.com>
2022-11-04 11:31:39 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,72 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :breed, :id
+  def initialize(name:, grade: id: nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
+  def self.drop_table
+    sql<<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DH[:conn].execute(sql)
+  end
+
+  def self.create_table
+    sql<<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save 
+    sql<<-SQL
+      INSERT INTO students(name, grade)
+      VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    self
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name:, grade:)
+    student.save
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], grade: row[3])
+  end
+
+  def find_by_name(name)
+    sql<<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1;
+    SQL
+    DB[:conn].execute(sql).map do|row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
+
+  def update
+    sql<<-SQL
+      UPDATE songs
+      SET name = ?, album = ?
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.album, self.name)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
